22e6437fe2eb86c17b7ac1736a2158ced45bc5c4
Carlos Ravelo <gandazgul@.(none)>
2009-08-26 01:36:11 -0400

    added content-type header to src.php added github repo to about.php updated settings for my server

--- about.php
+++ about.php
@@ -23,6 +23,8 @@
 <div id="main" style="padding-left: 20px">
 	<p>newMusicServer is a rewrite of mstServer 0.5, I decided to start anew with all the experiences 
 	I acumulated developing mstServer I am writing this one much cleaner.</p>
-	<p class="title">You can download the last version of mstServer from here:</p><a href="https://launchpad.net/mstserver">https://launchpad.net/mstserver</a><br /><br />
-	Not sure where to publish this code now, since I am using git for version control, I wish launchpad used git instead of bazaar. Maybe GitHub will do.
+	<p class="title">You can download the last version of mstServer from here(bazaar): </p>
+	<a href="https://launchpad.net/mstserver">https://launchpad.net/mstserver</a><br /><br />
+	<p class="title">You can download the lastest code as well as the last stable release of newMusicServer from here(git): </p>
+	<a href="git://github.com/gandazgul/newmusicserver.git">newMusicServer at GitHub</a><br /><br />
 </div>
--- src.php
+++ src.php
@@ -1,5 +1,6 @@
 <?php
 $srcFile = "src.zip";
+header("Content-Type: application/zip");
 header("Content-length: ".filesize($srcFile) );
 header("Content-Disposition: filename=\"".$srcFile."\"");
 header("Content-Transfer-Encoding: binary");
