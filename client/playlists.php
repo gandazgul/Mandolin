eab9393961dfd0b01dca964fdf367fae938dd882
unknown <gandazgul@.(none)>
2010-01-12 23:11:28 -0500

    .

--- client/playlists.php
+++ client/playlists.php
@@ -13,38 +13,39 @@
 </form>
 <div id="main">
 	<div class="subcolumns">
-	  <div class="c33l">
-	    <div class="subcl" style="padding-left: 20px; ">
+		<div class="c33l">
 			<p class="title">Saved Playlists:&nbsp;<span id="plTotal"></span></p>
-			<ol id="plList">
-				<li><img alt="Loading..." src="./client/images/ajax-loader.gif" /></li>
-			</ol>
-	    </div>
-	  </div>
-	  <div class="c33l">
-	    <div class="subcl">
-			<p class="title">List content:&nbsp;<span id="listName"></span></p>
-			<!--select id="plContents" size="20" style="width: 100%; " multiple="multiple"></select-->
-			<ol id="plContents"></ol>
-	    </div>
-	  </div>
-	  <div class="c33l">
-	    <div class="subcl" style="border-left: 1px dotted #DDDDDD; padding-left: 10px; padding-right: 20px; ">
-			<h6 class="vlist">Current Playlist Selection</h6>
-			<ul class="vlist">
-			  <li><a href="javascript:playPL()">Play Selected</a></li>
-  			  <li><a href="javascript:ranPlayPL()">Shuffle then Play</a></li>
-			  <li><a href="javascript:renPL()">Rename Playlist</a></li>			  
-			  <li><a href="javascript:delPL()">Delete Playlist</a></li>
-			</ul>
-			<h6 class="vlist">Current Song Selection</h6>
-			<ul class="vlist">
-				<li><a href="javascript:shuffle()">Shuffle Playlist</a></li>	
-			 	<li><a href="javascript:delFromPl()">Delete from playlist</a></li>
-			  	<li><a href="javascript:move(true)">Move up</a></li>
-			  	<li><a href="javascript:move(false)">Move down</a></li>
-			</ul>			
-	    </div>
-	  </div>
+			<div class="subcl musicList p20">
+				<ol id="plList">
+					<li><img alt="Loading..." src="./client/images/ajax-loader.gif" /></li>
+				</ol>
+			</div>
+		</div>
+		<div class="c33l">
+			<div class="subcl">
+				<p class="title">List content:&nbsp;<span id="listName"></span></p>
+				<div class="musicList">
+					<ol id="plContents"></ol>
+				</div>
+			</div>
+		</div>
+		<div class="c33l">
+			<div class="subcl" style="border-left: 1px dotted #DDDDDD; padding-left: 10px; padding-right: 20px; ">
+				<h6 class="vlist">Current Playlist Selection</h6>
+				<ul class="vlist">
+					<li><a href="javascript:playPL()">Play Selected</a></li>
+					<li><a href="javascript:ranPlayPL()">Shuffle then Play</a></li>
+					<li><a href="javascript:renPL()">Rename Playlist</a></li>
+					<li><a href="javascript:delPL()">Delete Playlist</a></li>
+				</ul>
+				<h6 class="vlist">Current Song Selection</h6>
+				<ul class="vlist">
+					<li><a href="javascript:shuffle()">Shuffle Playlist</a></li>
+					<li><a href="javascript:delFromPl()">Delete from playlist</a></li>
+					<li><a href="javascript:move(true)">Move up</a></li>
+					<li><a href="javascript:move(false)">Move down</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </div>
