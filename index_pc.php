8da0157fccbc590f14e047a844d8553e0c58e55e
Gandazgul <ravelo.carlos@gmail.com>
2010-03-25 00:14:08 -0400

    changed the doctype back to transitional, strict was causing a bug in the lists

--- index_pc.php
+++ index_pc.php
@@ -11,7 +11,8 @@
 
 	$username = (isset($_SESSION['username'])) ? $_SESSION['username'] : "guest";
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
 		<title>Mandolin v<?php echo $settings->get("version"); ?></title>
